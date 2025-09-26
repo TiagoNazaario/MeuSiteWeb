21ae62cdb7a9d01a803975106211d5f9c1eec185
Tiago <tiagosouza552f@gmail.com>
2025-09-25 21:42:44 -0300

    2 versao

--- main.py
+++ main.py
@@ -9,10 +9,17 @@ app = Flask(__name__)
 @app.route("/")
 def Homepage():
     return render_template("homepage.html")
+
 ##Pagina contatos
 @app.route("/contatos")
 def Contatos():
     return render_template("contatos.html")
+
+##pagina usuarios
+@app.route("/usuarios/<nome_usuario>")
+def usuarios(nome_usuario):
+    return render_template("usuarios.html", nome_usuario=nome_usuario)
+
 #colocar o site no ar
 if __name__ == "__main__":
     app.run(debug=True)
